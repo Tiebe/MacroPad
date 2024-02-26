761d5dbe0229e9eae8306ee6da031b3bb074269d
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-02-26 20:51:28 +0100

    changes

---
+++ include/main.h
@@ -0,0 +1,10 @@
+//
+// Created by tiebe on 2/26/24.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+bool isTiebeAwesome();
+
+#endif //MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <Arduino.h>
+#include "main.h"
+#include "SomfyRemote.h"
+
 void setup() {
 // write your initialization code here
 }
 
 void loop() {
 // write your code here
+    isTiebeAwesome();
+}
+
+bool isTiebeAwesome() {
+    if (0 == __null) {
+        return false;
+    }
 }
