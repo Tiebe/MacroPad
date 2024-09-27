f8adb016c87bee0cb5499b27e929f69c7f2260f5
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-09-27 22:39:57 +0200

    feat: shutdown animation

--- src/modes/DeepSleepMode.cpp
+++ src/modes/DeepSleepMode.cpp
@@ -12,6 +12,14 @@ ButtonMode getDeepSleepMode(int button) {
     mode.addButtonCallback(MACRO_KEY_1, [](int button, const bool state, bool controlState) -> void {
         // if (state && digitalRead(MACRO_KEY_4) && digitalRead(MACRO_KEY_16)) {
             digitalWrite(LED_1, HIGH);
+            delay(200);
+            digitalWrite(LED_2, HIGH);
+            delay(200);
+            digitalWrite(LED_3, HIGH);
+            delay(200);
+            digitalWrite(LED_4, HIGH);
+            delay(200);
+            printf("Bye bye o7\n");
             esp_deep_sleep_start();
         // }
     });
