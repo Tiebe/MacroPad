3609a52010a6df9b9cbe481b483ba82f0c58ea92
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-09-28 21:16:44 +0200

    Removed comment

--- src/modes/DeepSleepMode.cpp
+++ src/modes/DeepSleepMode.cpp
@@ -18,24 +18,22 @@ ButtonMode getDeepSleepMode(int button) {
     esp_sleep_enable_ext1_wakeup(mask, ESP_EXT1_WAKEUP_ANY_HIGH);
 
     mode.addButtonCallback(MACRO_KEY_1, [](int, const bool, bool) -> void {
-        // if (state && digitalRead(MACRO_KEY_4) && digitalRead(MACRO_KEY_16)) {
-            digitalWrite(LED_1, HIGH);
-            delay(200);
-            digitalWrite(LED_2, HIGH);
-            delay(200);
-            digitalWrite(LED_3, HIGH);
-            delay(200);
-            digitalWrite(LED_4, HIGH);
-            delay(200);
-            printf("Bye bye o7\n");
-            delay(1000);
-            digitalWrite(LED_1, LOW);
-            digitalWrite(LED_2, LOW);
-            digitalWrite(LED_3, LOW);
-            digitalWrite(LED_4, LOW);
+        digitalWrite(LED_1, HIGH);
+        delay(200);
+        digitalWrite(LED_2, HIGH);
+        delay(200);
+        digitalWrite(LED_3, HIGH);
+        delay(200);
+        digitalWrite(LED_4, HIGH);
+        delay(200);
+        printf("Bye bye o7\n");
+        delay(1000);
+        digitalWrite(LED_1, LOW);
+        digitalWrite(LED_2, LOW);
+        digitalWrite(LED_3, LOW);
+        digitalWrite(LED_4, LOW);
 
-            esp_deep_sleep_start();
-        // }
+        esp_deep_sleep_start();
     });
 
     return mode;
