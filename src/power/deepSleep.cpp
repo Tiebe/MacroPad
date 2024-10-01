111e49069a099f64175c3d907198f3a69f7f4322
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-10-01 20:01:57 +0200

    change: Made shutdown time variable

--- src/power/deepSleep.cpp
+++ src/power/deepSleep.cpp
@@ -12,11 +12,13 @@ bool led2 = false;
 bool led3 = false;
 bool led4 = false;
 
+#define SHUTDOWN_TIME 120
+
 void checkShutdown(const bool shuttingDown) {
     if (shuttingDown) {
         shutdownCounter++;
     } else {
-        if (shutdownCounter > 50) {
+        if (shutdownCounter > SHUTDOWN_TIME / 6) {
             digitalWrite(LED_1, led1);
             digitalWrite(LED_2, led2);
             digitalWrite(LED_3, led3);
@@ -25,23 +27,23 @@ void checkShutdown(const bool shuttingDown) {
         shutdownCounter = 0;
     }
 
-    if (shutdownCounter <= 50) {
+    if (shutdownCounter <= SHUTDOWN_TIME / 6) {
         led1 = digitalRead(LED_1);
         led2 = digitalRead(LED_2);
         led3 = digitalRead(LED_3);
         led4 = digitalRead(LED_4);
-    } else if (shutdownCounter <= 100) {
+    } else if (shutdownCounter <= SHUTDOWN_TIME / 3) {
         digitalWrite(LED_1, LOW);
         digitalWrite(LED_2, LOW);
         digitalWrite(LED_3, LOW);
         digitalWrite(LED_4, LOW);
-    } else if (shutdownCounter <= 150) {
+    } else if (shutdownCounter <= SHUTDOWN_TIME / 2) {
         digitalWrite(LED_1, HIGH);
-    } else if (shutdownCounter <= 200) {
+    } else if (shutdownCounter <= SHUTDOWN_TIME * 2 / 3) {
         digitalWrite(LED_2, HIGH);
-    } else if (shutdownCounter <= 250) {
+    } else if (shutdownCounter <= SHUTDOWN_TIME * 5 / 6) {
         digitalWrite(LED_3, HIGH);
-    } else if (shutdownCounter <= 300) {
+    } else if (shutdownCounter <= SHUTDOWN_TIME) {
         digitalWrite(LED_4, HIGH);
     } else {
         shutdown();
